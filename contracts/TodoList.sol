aa52e06fc61b4884a2a4feb1cbf41eb81f52f3e1
TrungyuD <TrungyuD>
2021-04-09 09:04:42 +0700

    first commit

---
+++ contracts/TodoList.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+  uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out dapp");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+
+    }
+
+}
+
